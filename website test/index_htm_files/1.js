41f856c4ad78d5031f5726b1ab4e379d99266f46
elementofaroma <elementofaroma@gmail.com>
2017-12-18 21:13:24 -0500

    web

---
+++ website test/index_htm_files/1.js
@@ -0,0 +1,38 @@
+window.local_simpleGallery3Columns_0_data = [
+{
+ dimpage: "Y",
+ showshadow: "Y"
+},
+{
+ image: "index_htm_files/141.jpg",
+ thumbnail: "index_htm_files/142.jpg"
+},
+{
+ image: "index_htm_files/143.jpg",
+ thumbnail: "index_htm_files/144.jpg"
+},
+{
+ image: "index_htm_files/145.jpg",
+ thumbnail: "index_htm_files/146.jpg"
+},
+{
+ image: "index_htm_files/147.jpg",
+ thumbnail: "index_htm_files/148.jpg"
+},
+{
+ image: "index_htm_files/149.jpg",
+ thumbnail: "index_htm_files/150.jpg"
+},
+{
+ image: "index_htm_files/151.jpg",
+ thumbnail: "index_htm_files/152.jpg"
+},
+{
+ image: "index_htm_files/153.jpg",
+ thumbnail: "index_htm_files/154.jpg"
+},
+{
+ image: "index_htm_files/155.jpg",
+ thumbnail: "index_htm_files/156.jpg"
+}
+];
