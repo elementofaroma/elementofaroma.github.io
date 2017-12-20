3ce8ca5e604d0f4b480b403f5b650e6302fc8ced
elementofaroma <elementofaroma@gmail.com>
2017-12-19 20:24:38 -0500

    update

--- index_htm_files/1.js
+++ index_htm_files/1.js
@@ -1,38 +1,38 @@
-window.local_simpleGallery3Columns_0_data = [
+window.local_simpleGallery3Columns_1_data = [
 {
  dimpage: "Y",
  showshadow: "Y"
 },
 {
  image: "index_htm_files/141.jpg",
- thumbnail: "index_htm_files/142.jpg"
+ thumbnail: "index_htm_files/160.jpg"
 },
 {
  image: "index_htm_files/143.jpg",
- thumbnail: "index_htm_files/144.jpg"
+ thumbnail: "index_htm_files/161.jpg"
 },
 {
  image: "index_htm_files/145.jpg",
- thumbnail: "index_htm_files/146.jpg"
+ thumbnail: "index_htm_files/162.jpg"
 },
 {
  image: "index_htm_files/147.jpg",
- thumbnail: "index_htm_files/148.jpg"
+ thumbnail: "index_htm_files/163.jpg"
 },
 {
  image: "index_htm_files/149.jpg",
- thumbnail: "index_htm_files/150.jpg"
+ thumbnail: "index_htm_files/164.jpg"
 },
 {
  image: "index_htm_files/151.jpg",
- thumbnail: "index_htm_files/152.jpg"
+ thumbnail: "index_htm_files/165.jpg"
 },
 {
  image: "index_htm_files/153.jpg",
- thumbnail: "index_htm_files/154.jpg"
+ thumbnail: "index_htm_files/166.jpg"
 },
 {
  image: "index_htm_files/155.jpg",
- thumbnail: "index_htm_files/156.jpg"
+ thumbnail: "index_htm_files/167.jpg"
 }
 ];
